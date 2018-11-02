fae937f5b52f542a5832f45037a93888492e6c8c
Nghia Tran <vinhnghiatran@gmail.com>
2018-11-02 16:55:01 +0700

    Remove User Promp

--- iosCertTrustManager.py
+++ iosCertTrustManager.py
@@ -42,27 +42,7 @@ def query_yes_no(question, default="yes"):
 
     The "answer" return value is one of "yes" or "no".
     """
-    valid = {"yes":"yes",   "y":"yes",  "ye":"yes",
-             "no":"no",     "n":"no"}
-    if default == None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while 1:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return default
-        elif choice in valid.keys():
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "\
-                             "(or 'y' or 'n').\n")
+    return "yes"
 
 #----------------------------------------------------------------------
 # A simple ASN1 decoder/encoder based on Python-ASN1
